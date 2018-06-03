d9a7f137cc1f0f443115edbd4b95bba47c542d39
Sebastien Mercier <pro.sebastienmercier@gmail.com>
2018-06-03 10:44:43 +0200

    Initial Commit

---
+++ GSB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GSB
+//
+//  Created by Sebastien Mercier on 03.06.18.
+//  Copyright © 2018 Galaxy Swiss Bourdin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
